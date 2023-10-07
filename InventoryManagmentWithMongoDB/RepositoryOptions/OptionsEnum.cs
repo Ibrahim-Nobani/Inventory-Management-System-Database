bad318bca8d3e85a164fd2a75e30b13e72822837
Ibrahim <inobani28@gmail.com>
2023-10-07 16:32:21 +0300

    Adding the MongoDB options

---
+++ InventoryManagmentWithMongoDB/RepositoryOptions/OptionsEnum.cs
@@ -0,0 +1,11 @@
+namespace InventoryOptions
+{
+    enum InventoryOptionsEnum
+    {
+        AddProductOption = 1,
+        DeleteProductOption = 2,
+        EditProductOption = 3,
+        SearchProductOption = 4,
+        ExitOption = 5
+    }
+}
